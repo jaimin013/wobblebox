45d16fd13eab5537623af0463863c660872e170e
Jaimin <jaiminkapadiya13@gmail.com>
2025-04-28 23:08:10 +0530

    fix

--- lib/db/migrate.ts
+++ lib/db/migrate.ts
@@ -5,20 +5,20 @@ import {neon} from "@neondatabase/serverless";
 import * as dotenv from "dotenv";
 dotenv.config({path: ".env.local"})
 
-if (process.env.DATABASE_URL!) {
+if (!process.env.DATABASE_URL) {
   throw new Error("database url is not set/conntected in.env.local")
 }
 
 async function runMigration() {
   try {
-    const sql = neon(process.env.DATABSE_URL!)
+    const sql = neon(process.env.DATABASE_URL!)
     const db = drizzle(sql)
 
     await migrate(db, {migrationsFolder: "./drizzle"});
     console.log("all migrations are succesfully done");
     
   } catch (error) {
-    console.log("all migrations are not done");
+    console.log("all migrations are done succesfully");
     process.exit(1)
   }
 }
